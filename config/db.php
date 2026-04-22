e6d2f88f722dd97582820b0ac65f3cc7a5cc9945
Eugene Yarney <eugeneyarney5@gmail.com>
2026-04-22 16:06:11 +0000

    Update database connection details in db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,8 @@
 <?php
 // Database configuration
 
-$conn = pg_connect("host=localhost dbname=syncbase user=postgres password=dbv5nono8rKKGMPf");
+$conn = pg_connect("host=aws-0-eu-west.pooler.supabase.com dbname=postgres user=
+postgres.ayubevnxrmxvamdwuecx password=x@0H_jnXU;j\lGZI");
 if (!$conn){
     die("Connection failed:   ". pg_last_error());
 }
