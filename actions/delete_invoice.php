f30b14aab09d4b22eaa4fdcde0544bd9090d078c
Eugene Yarney <eugeneyarney5@gmail.com>
2026-04-23 12:28:31 +0000

    Refactor delete_invoice.php to use pg_query_params

--- actions/delete_invoice.php
+++ actions/delete_invoice.php
@@ -2,21 +2,13 @@
 include("../includes/session.php");
 include("../includes/auth.php");
 
-if (!isLoggedIn()) {
-    header("Location: ../login.php");
-    exit();
-}
+if (!isLoggedIn()) { header("Location: ../login.php"); exit(); }
 
 $user_id = $_SESSION["user_id"];
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if (!$id) { header("Location: ../pages/invoices.php"); exit(); }
 
-if (!$id) {
-    header("Location: ../pages/invoices.php");
-    exit();
-}
-
-pg_prepare($conn, "delete_invoice", "DELETE FROM invoices WHERE id=$1 AND user_id=$2");
-pg_execute($conn, "delete_invoice", array($id, $user_id));
+pg_query_params($conn, "DELETE FROM invoices WHERE id=$1 AND user_id=$2", array($id, $user_id));
 
 header("Location: ../pages/invoices.php");
 exit();
