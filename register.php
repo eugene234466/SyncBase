ff9bfe21b7acad81dfe350b8407d71fe4041281e
Eugene Yarney <eugeneyarney5@gmail.com>
2026-04-23 11:22:16 +0000

    Fix registration form HTML and PHP logic

--- register.php
+++ register.php
@@ -46,6 +46,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Sekuya&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=Sekuya&family=Sora:wght@300;400;500;600&display=swap" rel="stylesheet">
         <title>Register - SyncBase</title>
     </head>
     <body>
