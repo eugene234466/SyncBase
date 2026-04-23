cd60dac6e499bea15952261a38332be4470427e4
Eugene Yarney <eugeneyarney5@gmail.com>
2026-04-23 12:20:11 +0000

    Refactor delete_email.php to use pg_query_params

--- actions/delete_email.php
+++ actions/delete_email.php
@@ -2,21 +2,13 @@
 include("../includes/session.php");
 include("../includes/auth.php");
 
-if (!isLoggedIn()) {
-    header("Location: ../login.php");
-    exit();
-}
+if (!isLoggedIn()) { header("Location: ../login.php"); exit(); }
 
 $user_id = $_SESSION["user_id"];
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if (!$id) { header("Location: ../pages/emails.php"); exit(); }
 
-if (!$id) {
-    header("Location: ../pages/emails.php");
-    exit();
-}
-
-pg_prepare($conn, "delete_email", "DELETE FROM emails WHERE id=$1 AND user_id=$2");
-pg_execute($conn, "delete_email", array($id, $user_id));
+pg_query_params($conn, "DELETE FROM emails WHERE id=$1 AND user_id=$2", array($id, $user_id));
 
 header("Location: ../pages/emails.php");
 exit();
