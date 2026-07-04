9f96257abf50f4426c5a4d68aeedf9980fa6b00b
Eugene Yarney <eugeneyarney5@gmail.com>
2026-07-04 13:24:13 +0000

    Refactor db.php to use environment variables
    Updated database connection string to use environment variables for host, dbname, user, and password.

--- config/db.php
+++ config/db.php
@@ -7,7 +7,7 @@ $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
-$conn = $conn = pg_connect("host=aws-0-eu-west-1.pooler.supabase.com port=5432 dbname=postgres user=postgres.ayubevnxrmxvamdwuecx password=Y7hBnPc6uS6EJmMVr");
+$conn = $conn = pg_connect("host=$host dbnamme=$dbname user=$user password=$password");
 if (!$conn){
     error_log("PostgreSQL connection failed");
     die("Unable to connect to database. Please try again later.");
